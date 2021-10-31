96319b53d6e60c6787dc017bb0a4d10c71844903
Aaron Tobias <adt27@uakron.edu>
2021-10-30 22:30:06 -0400

    added symbolize function

--- 06/Assembler/Assembler.py
+++ 06/Assembler/Assembler.py
@@ -5,12 +5,41 @@
 
 import sys
 
+REGISTERS = {
+    "RO": 0,
+    "R1":1,
+    "R2":2,
+    "R3":3,
+    "R4":4,
+    "R5":5,
+    "R6":6,
+    "R7":7,
+    "R8":8,
+    "R9":9,
+    "R10":10,
+    "R11":11,
+    "R12":12,
+    "R13":13,
+    "R14":14,
+    "R15":15,
+    "KBD":11111,
+    "SCREEN":8192,
+}
+
+TOKENS=[
+    "//",
+
+]
+SYMBOLS = []
+
+
 
 class Assembler():
     def __init__(self):
         self.parse_arguments()
         with open(self.filename, 'r') as file:
             self.asm = file.readlines()
+        self.symbols=[]
 
     def __repr__(self):
         '''
@@ -25,6 +54,9 @@ class Assembler():
         return ''.join(self.bytcode)
 
     def parse_arguments(self):
+        '''
+        Parses command line arguments into filename
+        '''
         if len(sys.argv) != 2:
             print(f"""\nERROR: Missing filename. \nUsage: {sys.argv[0]} /path/to/file.asm
             """)
@@ -32,7 +64,21 @@ class Assembler():
         else:
             self.filename = sys.argv[1]
             print(f"Assembling {self.filename}")
-
-
-asm = Assembler()
-print(repr(asm))
+    
+    def symbolize(self):
+        """
+        Converts ASM to it's base symbols
+        Removes comments and whitespace
+        """
+        #Check every line
+        for line in self.asm:
+            # Check for comments and remove
+            comment=line.rfind("//")
+            line=line[:comment]
+            # Remove whitespace
+            line=line.strip()
+            # Remove empty lines after stripping
+            if len(line) == 0:
+                continue
+            # Store whatever is left
+            self.symbols.append(line)
